6fa8aff50b8447971752bac207eeca8b0ec7a4a7
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-05-24 14:12:51 -0300

    upload ok

--- app/Models/Buraco.php
+++ app/Models/Buraco.php
@@ -6,19 +6,27 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Buraco extends BaseModel
 {
     use SoftDeletes;
-    
+
     public function cadastro($request)
     {
         if($request->photo){
-            $img_name = date("HisdmY");
-            $request->file('photo')->move('img/buracos', "$img_name.jpeg");
+            // $img_name = date("HisdmY");
+            // $request->file('photo')->move('img/buracos', "$img_name.jpeg");
+
+            define('UPLOAD_DIR', 'img/buracos/');
+            $image_parts = explode(";base64,", $request->photo);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+            $image_base64 = base64_decode($image_parts[1]);
+            $file_name = UPLOAD_DIR . uniqid() . '.jpeg';
+            file_put_contents($file_name, $image_base64);
         } else {
-            $img_name = 'semnome';
+            $file_name = null;
         }
 
         Buraco::create([
             'street' => $request->street,
-            'photo' => $img_name . '.jpeg',
+            'photo' => $file_name,
         ]);
     }
 }
